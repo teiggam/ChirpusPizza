08e47066acda6f021f5ad2e861238700c7b6ee18
Maggie Tamanini <82274835+teiggam@users.noreply.github.com>
2021-07-12 16:05:18 -0400

    Changing files...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
+import { Component, Pipe } from '@angular/core';
+import { Observable } from 'rxjs';
+import {MenuItem} from './menuItem';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChirpusPizza';
+  title = 'Chirpus Pizza';
+
+  menu: MenuItem[] = [
+    {name: "Large Pizza", category: "Dinner", price: 15.99},
+    {name: "Cheese Bread", category: "Sides", price: 5.99},
+    {name:"Garlic Knots", category: "Sides", price: 3.99},
+    {name:"2 Liter of Pepsi", category: "Drinks", price: 2.99},
+    {name:"Red Bull", category: "Drinks", price: 3.99},
+    {name:"Personal Pizza", category:"Dinner", price: 5.99},
+    {name:"Chicken Wings", category: "Dinner", price: 7.99}
+  ]
+ 
+
+  
 }
+
+
